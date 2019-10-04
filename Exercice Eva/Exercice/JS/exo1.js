8d52ed94d39e8adda4882a8509ddf54e85a63f98
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-10-04 15:06:31 +0200

    Update

--- Exercice Eva/Exercice/JS/exo1.js
+++ Exercice Eva/Exercice/JS/exo1.js
@@ -1,9 +1,16 @@
-document.addEventListener('DOMContentLeaded', init)
+/*
+    Programme : Exercice Eva
+    Auteur : Thomas Grossmann
+    Date : 04.10.2019
+*/
+document.addEventListener('DOMContentLoaded', init)
 function init() {
     bluetraced.addEventListener('click', Div1Click)
     clearDiv1.addEventListener('click', ClearDiv1)
     fontArial.addEventListener('mouseover', FontArial)
     fontArial.addEventListener('mouseleave', FontArial2)
+    YES.addEventListener('click', Yes)
+    Clicktext.addEventListener('click', ClickText)
 }
 
 function Div1Click() {
@@ -24,4 +31,15 @@ function FontArial2() {
     fontArial.style.fontFamily = ""
 }
 
+function Yes() {
+    if(YES1.checked&&YES2.checked){
+        YES.style.backgroundColor="green"               //
+        YES.style.color="white"
+        YES.style.border="solid 0px black"                                  //Retirer la bordure
+        document.getElementById("YES").innerHTML=("<p>YES<\p>")
+    }
+}
 
+function ClickText() {
+    Delete.style.display = "none"
+}
