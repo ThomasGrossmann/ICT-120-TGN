002b4e69f9af644e3313d8eaccea2964aa47ffcb
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-10-07 16:00:10 +0200

    Update

--- Exercice Eva/Exercice/JS/exo1.js
+++ Exercice Eva/Exercice/JS/exo1.js
@@ -11,6 +11,10 @@ function init() {
     fontArial.addEventListener('mouseleave', FontArial2)
     YES.addEventListener('click', Yes)
     Clicktext.addEventListener('click', ClickText)
+    nomprenom.addEventListener('keyup', Initiales)
+    partez.style.display="none"
+    avosmarques.addEventListener('click', AVosMarques)
+    pret.addEventListener('click', Pret)
 }
 
 function Div1Click() {
@@ -33,9 +37,9 @@ function FontArial2() {
 
 function Yes() {
     if(YES1.checked&&YES2.checked){
-        YES.style.backgroundColor="green"               //
+        YES.style.backgroundColor="green"
         YES.style.color="white"
-        YES.style.border="solid 0px black"                                  //Retirer la bordure
+        YES.style.border="solid 0px black"
         document.getElementById("YES").innerHTML=("<p>YES<\p>")
     }
 }
@@ -43,3 +47,32 @@ function Yes() {
 function ClickText() {
     Delete.style.display = "none"
 }
+
+function Initiales() {
+    var Initiales = ""
+    var Espace = 0
+    var Longueur = ""
+    Initiales = nomprenom.value.substr(0,1)
+    Espace = nomprenom.value.lastIndexOf(" ")
+    Initiales = Initiales + nomprenom.value.substr(Espace+1, 1)
+    Longueur = nomprenom.value.length
+    Initiales = Initiales + nomprenom.value.substr(Longueur -1, 1)
+    initiales.value = Initiales.toUpperCase()
+}
+
+function AVosMarques() {
+    avosmarque.disabled = true
+    pret.disabled = false
+    partez.style.display = "inline"
+    partez.disabled = true
+    myImage1.src="Images/marks.png"
+
+}
+
+/*function Pret() {
+    avosmarques.style.display = "none"
+    pret.disabled = true
+    partez.disabled = false
+    myImage1.src="Images/set.png"
+}
+ */
