0eecd06b6b25d8920d70159c1eb7996261cfc58b
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-10-31 16:57:03 +0100

    Update

---
+++ Projet/JS/Projet_JS.js
@@ -0,0 +1,12 @@
+/*
+    Programme : Projet_JS
+    Auteur : Thomas Grossmann
+    Date : 31.10.2019
+*/
+document.addEventListener('DOMContentLoaded', init)
+function init() {
+    heb.addEventListener('click', clickHebergement())
+}
+function clickHebergement() {
+    content.style.display = "block"
+}
