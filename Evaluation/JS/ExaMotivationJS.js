81bb20872e8e0da8fb19efa6ba11808c01b066d4
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-11-15 10:34:41 +0100

    Exam FIN

---
+++ Evaluation/JS/ExaMotivationJS.js
@@ -0,0 +1,15 @@
+/*
+    Projet : Examen ICT 120
+    Auteur : Thomas Grossmann
+    Date : 15.11.2019
+*/
+
+document.addEventListener('DOMContentLoaded', init)
+
+function init() {
+    alert(window.location.search)
+}
+
+function BtnOK() {
+    if ()
+}
